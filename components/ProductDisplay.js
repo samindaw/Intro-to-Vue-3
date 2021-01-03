c9aab58541eebdc34c40750a8d8d21d54f3bb30c
Saminda Wijeratne <samindaw@gmail.com>
2021-01-03 22:47:41 +0530

    done

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -39,6 +39,11 @@ app.component('product-display', {
           v-on:click="addToCart">
           Add to Cart
         </button>
+        <button 
+          class="button" 
+          v-on:click="removeFromCart">
+          Remove Cart
+        </button>
       </div>
     </div>
   </div>`,
@@ -50,13 +55,16 @@ app.component('product-display', {
         details: ['50% cotton', '30% wool', '20% polyester'],
         variants: [
           { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-          { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+          { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 2 },
         ]
     }
   },
   methods: {
       addToCart() {
-          this.cart += 1
+          this.$emit("new-stuff", this.variants[this.selectedVariant].id)
+      },
+      removeFromCart(){
+        this.$emit("del-stuff", this.variants[this.selectedVariant].id);
       },
       updateVariant(index) {
           this.selectedVariant = index
